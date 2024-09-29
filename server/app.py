43dce207d5e8ca0644a51e3e97fd8554eb44d770
Thomas <37830237+thomaslin2020@users.noreply.github.com>
2024-09-29 09:26:55 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -94,4 +94,4 @@ async def info(settings: Annotated[config.Settings, Depends(get_settings)]):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
